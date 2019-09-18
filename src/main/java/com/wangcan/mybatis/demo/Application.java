bb7799ad25dac72fbe9b7800f7692239157b1d77
wangc <wangcan4096505@qq.com>
2019-09-18 21:20:05 +0800

    引入log

--- src/main/java/com/wangcan/mybatis/demo/Application.java
+++ src/main/java/com/wangcan/mybatis/demo/Application.java
@@ -20,12 +20,15 @@ public class Application {
             InputStream inputStream = Resources.getResourceAsStream(resource);
             SqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream,"dev");
             SqlSession sqlSession = SqlSessionFactory.openSession();
+            SqlSessionFactory.getConfiguration();
             //直接掉用
 //            Integer id = sqlSession.selectOne("com.wangcan.mybatis.demo.mapper.UserMapper.select", 1);
 //            System.out.println(id);
             //通过接口调用
             UserMapper mapper = sqlSession.getMapper(UserMapper.class);
             User user = mapper.select(1);
+            //测试本地缓存
+            User user1 = mapper.select(1);
             System.out.println(user);
 
         } catch (Exception e) {
